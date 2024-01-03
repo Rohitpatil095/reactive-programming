541cc7da8bb55018e8419e0b6d4d0a00fa9256c4
Rohitpatil095 <rohitpatil1340sa@gmail.com>
2024-01-03 20:43:53 +0530

    experiment with reactive db methods

---
+++ src/main/java/com/app/reactive/ReactiveDbApplication.java
@@ -0,0 +1,13 @@
+package com.app.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDbApplication.class, args);
+	}
+
+}
